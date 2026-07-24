ae03808bd6ac650c0d204d6a8ef87377a275b41f
Derick Canceran <derickcanceran.123@gmail.com>
2026-07-24 16:48:17 +0800

    Spring Initializr files

---
+++ src/test/java/com/github/derick01/pantry_tracker/PantryTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.derick01.pantry_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
